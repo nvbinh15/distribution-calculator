1fb66dc30c9101c073bdcc56357709c0b1f869d3
nvbinh15 <binh.pdc01@gmail.com>
2021-11-17 23:38:57 +0800

    Add discrete random variable calculator

--- distribution.py
+++ distribution.py
@@ -7,6 +7,7 @@ from distributions.normal import normal
 from distributions.poisson import poisson
 from distributions.t import t
 from stats.sample import sample_params
+from stats.discrete_random_variable import discrete_random_variable_params as discrete
 
 p = f(12, 10)
 print(p)
@@ -18,3 +19,8 @@ print("-------------")
 
 samples = [10.2, 9.7, 10.1, 10.3, 10.1, 9.8, 9.9, 10.4, 10.3, 9.8]
 sample_params(samples)
+print("-------------")
+pmf = {2:0.01, 3:0.25, 4:0.4, 5:0.3, 6:0.04}
+discrete(pmf)
+
+
---
+++ stats/discrete_random_variable.py
@@ -0,0 +1,15 @@
+def discrete_random_variable_params(pmf):
+    expectation = 0
+    second_moment = 0
+    total_probability = sum(pmf[x] for x in pmf)
+    if (total_probability != 1):
+        print(f"The total probability is not 1. ({total_probability})")
+        return
+    print(f"Probability: {total_probability}")
+    for sample_point in pmf:
+        expectation += sample_point * pmf[sample_point]
+        second_moment += pmf[sample_point] * sample_point * sample_point
+    variance = second_moment - expectation ** 2
+    print(f"Expectation E(X) = mu = {expectation}")
+    print(f"Second moment E(X^2) = {second_moment}")
+    print(f"Variance V(X) = {variance}")
